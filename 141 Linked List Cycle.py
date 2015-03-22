0f2f8815cc172178c25405ad97492d6d5f753189
Wangshuo <wangshuo@WangshuodeMacBook-Pro.local>
2015-03-22 17:53:24 +0800

    141

---
+++ 141 Linked List Cycle.py
@@ -0,0 +1,45 @@
+#-*-coding:utf-8-*-
+__author__ = 'wangshuo'
+
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+#解法1： 每到一个节点把该节点的 next 弄成头，判断下一个节点的 next 的 next是不是头，如果是则有环
+class Solution:
+    # @param head, a ListNode
+    # @return a boolean
+    def hasCycle(self, head):
+        first = head
+        while head:
+            temp = head.next
+            head.next = head
+            head = temp
+            if not temp or not temp.next :
+                return False
+            if temp.next.next == first:
+                return True
+
+        return False
+
+
+#解法2：一个快指针 fast，一次跳两下，一个慢指针 slow 一次跳一下，如果有环，则两指针会相遇，如果 fast 走到末尾则没有环
+class Solution1:
+    # @param head, a ListNode
+    # @return a boolean
+    def hasCycle(self, head):
+        if not head:
+            return False
+        fast = head.next
+        slow = head
+        while fast:
+            if fast.next == None:
+                return False
+            if fast == slow:
+                return True
+            fast = fast.next.next
+            slow = slow.next
+
+        return False
